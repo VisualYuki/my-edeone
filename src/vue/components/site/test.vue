7ca0b6c0170941766281a6f73a3075236f5f7c11
Moshkin Denis <moshkin.denis.007@yandex.ru>
2021-11-10 16:17:32 +0300

    test eslint

---
+++ src/vue/components/site/test.vue
@@ -0,0 +1,19 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+	export default {
+		name: "app",
+		data() {
+			return {
+				msg: "Welcome to Your Vue.js App",
+			};
+		},
+		props: {
+			propA: Number,
+		},
+	};
+</script>
+
+<style lang="scss"></style>
